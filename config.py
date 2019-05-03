be17ff0e58a93d612f4766f3e24fa5848321beba
Angus Munro <angus.munro@gmail.com>
2019-05-03 19:09:24 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+
+train_dir = 'data/train'
+val_dir   = 'data/validate'
+test_dir  = 'data/test'
+
+batch_size = 20
+logfile    = 'train.log'
+
+last       = 0
+learn_rate = 0.01
+lr_decay   = 0.9
+iterations = 5
